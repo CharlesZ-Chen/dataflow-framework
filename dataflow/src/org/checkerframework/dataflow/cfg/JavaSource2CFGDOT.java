e92aab3c5f1b1453f75ed4df6dc6243e5268d63b
CharlesZ-Chen <charleszhuochen@gmail.com>
2016-07-02 17:56:41 -0400

    fix bugs in JavaSource2CFGDOT, let it works again, and do a refactor.

--- dataflow/src/org/checkerframework/dataflow/cfg/JavaSource2CFGDOT.java
+++ dataflow/src/org/checkerframework/dataflow/cfg/JavaSource2CFGDOT.java
@@ -11,21 +11,26 @@ import org.checkerframework.dataflow.analysis.TransferFunction;
 import org.checkerframework.javacutil.BasicTypeProcessor;
 import org.checkerframework.javacutil.TreeUtils;
 
+
 import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintStream;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Map.Entry;
 
+import javax.annotation.processing.SupportedAnnotationTypes;
+import javax.lang.model.SourceVersion;
 import javax.lang.model.element.ExecutableElement;
+import javax.lang.model.element.TypeElement;
 import javax.tools.JavaFileManager;
 import javax.tools.JavaFileObject;
 import javax.xml.ws.Holder;
 
+import com.sun.source.tree.ClassTree;
 import com.sun.source.tree.CompilationUnitTree;
 import com.sun.source.tree.MethodTree;
+import com.sun.source.util.TreePath;
 import com.sun.source.util.TreePathScanner;
 import com.sun.tools.javac.file.JavacFileManager;
 import com.sun.tools.javac.main.JavaCompiler;
@@ -87,7 +92,7 @@ public class JavaSource2CFGDOT {
             System.exit(1);
         }
 
-        generateDOTofCFG(input, output, method, clas, pdf);
+        generateDOTofCFGWithoutAnalysis(input, output, method, clas, pdf);
     }
 
     /** Print an error message. */
@@ -110,7 +115,7 @@ public class JavaSource2CFGDOT {
     }
 
     /** Just like method above but without analysis. */
-    public static void generateDOTofCFG(String inputFile, String outputDir,
+    public static void generateDOTofCFGWithoutAnalysis(String inputFile, String outputDir,
             String method, String clas, boolean pdf) {
         generateDOTofCFG(inputFile, outputDir, method, clas, pdf, null);
     }
@@ -135,25 +140,12 @@ public class JavaSource2CFGDOT {
     void generateDOTofCFG(
             String inputFile, String outputDir, String method, String clas,
             boolean pdf, /*@Nullable*/ Analysis<A, S, T> analysis) {
-        Entry<MethodTree, CompilationUnitTree> m = getMethodTreeAndCompilationUnit(inputFile, method, clas);
-        generateDOTofCFG(inputFile, outputDir, method, clas, pdf, analysis, m.getKey(), m.getValue());
-    }
 
-    public static
-    <A extends AbstractValue<A>, S extends Store<S>, T extends TransferFunction<A, S>>
-    void generateDOTofCFG(
-            String inputFile, String outputDir, String method, String clas,
-            boolean pdf, /*@Nullable*/ Analysis<A, S, T> analysis, MethodTree m,
-            CompilationUnitTree r) {
+        ControlFlowGraph cfg = getMethodCFG(inputFile, method, clas);
+
         String fileName = (new File(inputFile)).getName();
         System.out.println("Working on " + fileName + "...");
 
-        if (m == null) {
-            printError("Method not found.");
-            System.exit(1);
-        }
-
-        ControlFlowGraph cfg = CFGBuilder.build(r, null, m, null);
         if (analysis != null) {
             analysis.performAnalysis(cfg);
         }
@@ -177,10 +169,12 @@ public class JavaSource2CFGDOT {
      */
     protected static void producePDF(String file) {
         try {
-            String command = "dot -Tpdf \"" + file + ".txt\" -o \"" + file
+            String command = "/usr/local/bin/dot -Tpdf \"" + file + "\" -o \"" + file
                     + ".pdf\"";
             Process child = Runtime.getRuntime().exec(command);
             child.waitFor();
+            System.out.println("generating pdf, command is:\n" + command);
+            System.out.println("success!");
         } catch (InterruptedException | IOException e) {
             e.printStackTrace();
             System.exit(1);
@@ -188,50 +182,20 @@ public class JavaSource2CFGDOT {
     }
 
     /**
-     * @return the AST of a specific method in a specific class in a specific
-     *         file (or null if no such method exists)
-     */
-    public static /*@Nullable*/ MethodTree getMethodTree(String file,
-            final String method, String clas) {
-        return getMethodTreeAndCompilationUnit(file, method, clas).getKey();
-    }
-
-    /**
      * @return the AST of a specific method in a specific class as well as the
      *         {@link CompilationUnitTree} in a specific file (or null they do
      *         not exist).
      */
-    public static Entry</*@Nullable*/ MethodTree, /*@Nullable*/ CompilationUnitTree> getMethodTreeAndCompilationUnit(
+    public static ControlFlowGraph getMethodCFG(
             String file, final String method, String clas) {
-        final Holder<MethodTree> m = new Holder<>();
-        final Holder<CompilationUnitTree> c = new Holder<>();
-        BasicTypeProcessor typeProcessor = new BasicTypeProcessor() {
-            @Override
-            protected TreePathScanner<?, ?> createTreePathScanner(
-                    CompilationUnitTree root) {
-                c.value = root;
-                return new TreePathScanner<Void, Void>() {
-                    @Override
-                    public Void visitMethod(MethodTree node, Void p) {
-                        ExecutableElement el = TreeUtils
-                                .elementFromDeclaration(node);
-                        if (el.getSimpleName().contentEquals(method)) {
-                            m.value = node;
-                            // stop execution by throwing an exception. this
-                            // makes sure that compilation does not proceed, and
-                            // thus the AST is not modified by further phases of
-                            // the compilation (and we save the work to do the
-                            // compilation).
-                            throw new RuntimeException();
-                        }
-                        return null;
-                    }
-                };
-            }
-        };
+
+        final Holder<ResultType> resultHolder = new Holder<>();
+        BasicTypeProcessor typeProcessor = new MyProcessor(clas, method, resultHolder);
 
         Context context = new Context();
+
         JavaCompiler javac = new JavaCompiler(context);
+
         javac.attrParseOnly = true;
         JavacFileManager fileManager = (JavacFileManager) context
                 .get(JavaFileManager.class);
@@ -254,22 +218,131 @@ public class JavaSource2CFGDOT {
         } finally {
             System.setErr(err);
         }
-        return new Entry<MethodTree, CompilationUnitTree>() {
-            @Override
-            public CompilationUnitTree setValue(CompilationUnitTree value) {
-                return null;
+
+        ResultType res = resultHolder.value;
+
+        if (res == null) {
+            printError("internal error in type processor! method typeProcess() doesn't get called.");
+            System.exit(1);
+        }
+
+        if (!res.isSuccess) {
+            printError(res.errMsg);
+            System.exit(1);
+        }
+
+        return res.getCFG();
+    }
+
+    @SupportedAnnotationTypes("*")
+    static class MyProcessor extends BasicTypeProcessor {
+
+        private final String className;
+        private final String methodName;
+
+        private final Holder<CompilationUnitTree> rootTree;
+        private final Holder<ClassTree> classTree;
+        private final Holder<MethodTree> methodTree;
+
+        private final Holder<ResultType> result;
+
+        MyProcessor(String className, String methodName, Holder<ResultType> res) {
+            this.className = className;
+            this.methodName = methodName;
+            this.rootTree = new Holder<> ();
+            this.methodTree = new Holder<> ();
+            this.classTree = new Holder<> ();
+            this.result = res;
+        }
+
+        @Override
+        public void typeProcess(TypeElement e, TreePath p) {
+            super.typeProcess(e, p);
+            if (rootTree.value == null) {
+                this.result.value = new ResultType(null, false, "rootTree is null!.");
+                return;
             }
 
-            @Override
-            public CompilationUnitTree getValue() {
-                return c.value;
+            if (classTree.value == null) {
+                this.result.value = new ResultType(null, false, "class not found.");
+                return;
             }
 
-            @Override
-            public MethodTree getKey() {
-                return m.value;
+            if (methodTree.value == null) {
+              this.result.value = new ResultType(null, false, "method not found.");
+              return;
             }
-        };
+
+            ControlFlowGraph cfg =
+                    CFGBuilder.build(rootTree.value, processingEnv, methodTree.value, classTree.value);
+            this.result.value = new ResultType(cfg);
+        }
+
+        @Override
+        protected TreePathScanner<?, ?> createTreePathScanner(CompilationUnitTree root) {
+            rootTree.value = root;
+            return new TreePathScanner<Void, Void>() {
+                @Override
+                public Void visitClass(ClassTree node, Void p) {
+                    TypeElement el = TreeUtils
+                            .elementFromDeclaration(node);
+                    if (el.getSimpleName().contentEquals(className)) {
+                        classTree.value = node;
+                    }
+                    return super.visitClass(node, p);
+                }
+
+                @Override
+                public Void visitMethod(MethodTree node, Void p) {
+                    ExecutableElement el = TreeUtils
+                            .elementFromDeclaration(node);
+                    if (el.getSimpleName().contentEquals(methodName)) {
+                        methodTree.value = node;
+                        // stop execution by throwing an exception. this
+                        // makes sure that compilation does not proceed, and
+                        // thus the AST is not modified by further phases of
+                        // the compilation (and we save the work to do the
+                        // compilation).
+                        throw new RuntimeException();
+                    }
+                    return null;
+                }
+            };
+        }
+
+        @Override
+        public SourceVersion getSupportedSourceVersion() {
+          return SourceVersion.latestSupported();
+        }
+    }
+
+    static class ResultType {
+        private final ControlFlowGraph controlFlowGraph;
+        private final boolean isSuccess;
+        private final String errMsg;
+
+        public ResultType(final ControlFlowGraph cfg) {
+            this(cfg, true, null);
+            assert cfg != null : "this constructor should called if cfg were success built.";
+        }
+
+        public ResultType(ControlFlowGraph cfg, boolean isSuccess, String errMsg) {
+            this.controlFlowGraph = cfg;
+            this.isSuccess = isSuccess;
+            this.errMsg = errMsg;
+        }
+
+        public boolean isSuccess() {
+            return isSuccess;
+        }
+
+        public ControlFlowGraph getCFG() {
+            return controlFlowGraph;
+        }
+
+        public String getErrMsg() {
+            return errMsg;
+        }
     }
 
 }
